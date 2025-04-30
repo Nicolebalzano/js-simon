3b32f5c1e65892de898d80fb2b8b7ad61713ec57
Nicolebalzano <nicole.balzano03@gmail.com>
2025-04-30 21:43:24 +0200

    feat:5 random numbers created

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente 
+// deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare
+//  sono stati individuati.
+// **NOTA**: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
+// **BONUS:**
+// - Inseriamo la validazione: se l'utente inserisce cose diverse da numeri lo blocchiamo in qualche modo.
+// - Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
+/**
+ * MILESTONE 0
+ * 1) Creare l'interfaccia 
+ * MILESTONE 1
+ * 2) Generare i 5 numeri random
+ * 3) Stampare i 5 numeri in pagina
+ * 4) Far scomparire i numeri dopo 30 secondi e mostrare gli input
+ * MILESTONE 2
+ * 5) Al click sul bottone leggere i numeri inseriti dall'utente
+ * 6) Controllare quanti dei numeri sono stati indovinati
+ * 7) Stampare il risultato del punteggio
+ */
+
+let count = 5;
+let min = 1;
+let max = 100;
+let numbers = [];
+function randomNumber (count, max, min) {
+       for (let i = 0; i < count; i++){
+              let random = (Math.floor(Math.random() * (max - min) + min));
+              numbers.push(random);
+       }
+       return numbers;
+}
+const numsRandom = randomNumber(count, max, min);
+console.log(numsRandom);
+
+    
+       
+     
